5e38076a546ded3b7736a69a134373cbdf059dc0
shubhamkailashkukku <shubhamkailashkukku@gmail.com>
2022-12-01 23:02:50 +0530

    End of section 3

--- src/app/Layout/App.jsx
+++ src/app/Layout/App.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
 
-function App() {
+export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-      <h1>Re-Vents</h1>
-      
-    </div>
-  );
+    <>
+      <NavBar setFormOpen={setFormOpen}/>
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+      </Container>
+    </>
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css'
 import './app/Layout/styles.css';
 import App from './app/Layout/App';
 import reportWebVitals from './reportWebVitals';
